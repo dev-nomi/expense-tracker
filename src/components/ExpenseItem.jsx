5d91476171439f82781b9a242386b6a3f56f21e1
dev-nomi <noman.khalid.freelance@gmail.com>
2023-06-01 01:34:38 +0500

    Added wrapper component Card

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,8 +1,9 @@
+import Card from "./Card";
 import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem({ title, amount, date }) {
   return (
-    <div className="md:flex text-center justify-between items-center p-2 my-4 mx-4 rounded-lg shadow-lg bg-cyan-700 text-white">
+    <Card className="md:flex text-center justify-between items-center p-3 my-4 mx-4 bg-cyan-700 text-white">
       <div className="md:flex md:justify-center md:items-center md:gap-4">
         <ExpenseDate date={date} />
         <div className="text-xl font-bold">{title}</div>
@@ -10,7 +11,7 @@ function ExpenseItem({ title, amount, date }) {
       <div className="bg-amber-300 border-2 border-white-200 font-bold py-2 px-4 rounded-lg">
         ${amount}
       </div>
-    </div>
+    </Card>
   );
 }
 
--- src/components/Expenses.jsx
+++ src/components/Expenses.jsx
@@ -1,13 +1,14 @@
+import Card from "./Card";
 import ExpenseItem from "./ExpenseItem";
 
 function Expenses({ expenses }) {
   return (
-    <div className="w-1/2 mx-auto my-8 shadow-lg shadow-slate-400 rounded-lg p-1">
+    <Card className="w-1/2 mx-auto my-8 p-1">
       <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} />
       <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} />
       <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} />
       <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date} />
-    </div>
+    </Card>
   );
 }
 
