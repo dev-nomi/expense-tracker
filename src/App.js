5d48edb5be620a86f7f9fb51e9f6affeaba517d8
dev-nomi <noman.khalid.freelance@gmail.com>
2023-05-31 21:28:29 +0500

    Added tailwind css into project

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import ExpenseItem from './components/ExpenseItem';
 function App() {
   return (
     <div>
-      <h1>Let's get started!</h1>
+      <h1 className="text-3xl font-bold">Let's get started!</h1>
       <ExpenseItem></ExpenseItem>
     </div>
   );
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,7 +3,7 @@ function ExpenseItem() {
     <div>
       <div>March 28th 2021</div>
       <div>
-        <h2>Car Insurance</h2>
+        <h2 className="text-xl font-bold">Car Insurance</h2>
         <div>$294.67</div>
       </div>
     </div>
